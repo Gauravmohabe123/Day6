ef9ef2e2c224aea2bdcccaeb4750bc02c0237587
Gauravmohabe123 <121918590+Gauravmohabe123@users.noreply.github.com>
2023-01-09 20:51:18 +0530

    Day6  assignment problem

---
+++ 1comnd_power.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+echo "Enter a no. and it will be print a table power of two that are less than and equal to 2^n"
+
+read n
+num=2
+
+for(( i<=1 ; i=$num ; i++ ))
+
+do
+
+	table=$(($i*$num))
+echo "table"
+done
